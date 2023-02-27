4b41fcb676f52123b7b2d3dd07b8b8dc4423b054
kangshang23 <kangshang23@gmail.com>
2023-02-27 20:10:56 +0900

    [Feat] Create ResponseHistory.java

---
+++ src/main/java/kang/zero/wifiyeah/dto/response/ResponseHistory.java
@@ -0,0 +1,24 @@
+package kang.zero.wifiyeah.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+public class ResponseHistory {
+    private Integer id;
+    private Float x;
+    private Float y;
+    private Timestamp createdTime;
+
+    @Builder
+    public ResponseHistory(Integer id, Float x, Float y, Timestamp createdTime) {
+        this.id = id;
+        this.x = x;
+        this.y = y;
+        this.createdTime = createdTime;
+    }
+}
