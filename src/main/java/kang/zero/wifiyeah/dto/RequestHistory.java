635c3700a0d31f9dce022aee509076f6790c91d9
kangshang23 <kangshang23@gmail.com>
2023-02-27 20:08:23 +0900

    [Chore] Create RequestHistory.java

---
+++ src/main/java/kang/zero/wifiyeah/dto/RequestHistory.java
@@ -0,0 +1,22 @@
+package kang.zero.wifiyeah.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+public class RequestHistory {
+    private Float x;
+    private Float y;
+    private Timestamp createdTime;
+
+    @Builder
+    public RequestHistory(Float x, Float y, Timestamp createdTime) {
+        this.x = x;
+        this.y = y;
+        this.createdTime = createdTime;
+    }
+}
