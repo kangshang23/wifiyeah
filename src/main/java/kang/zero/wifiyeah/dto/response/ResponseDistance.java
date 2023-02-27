c2e5cc4195079d0f48088ee9aa4e1cc594d6746f
kangshang23 <kangshang23@gmail.com>
2023-02-27 20:11:32 +0900

    [Feat] Create ResponseDistance.java

---
+++ src/main/java/kang/zero/wifiyeah/dto/response/ResponseDistance.java
@@ -0,0 +1,18 @@
+package kang.zero.wifiyeah.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ResponseDistance {
+    private Double distance;
+    private String manageNum;
+
+    @Builder
+    public ResponseDistance(Double distance, String manageNum) {
+        this.distance = distance;
+        this.manageNum = manageNum;
+    }
+}
