6bbf1f190ddbfc7492f90123cf70ab8282927f94
kangshang23 <kangshang23@gmail.com>
2023-03-01 16:47:34 +0900

    [Feat] Modify getHistoryList() in Service.java

--- src/main/java/kang/zero/wifiyeah/service/Service.java
+++ src/main/java/kang/zero/wifiyeah/service/Service.java
@@ -12,6 +12,7 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class Service {
     final static MySqlConfig dbConn = new MySqlConfig();
 
@@ -79,7 +80,10 @@ public class Service {
         conn.close();
     }
 
-    public void saveHistory(RequestHistory requestHistory) {
+    public void saveHistory(RequestHistory requestHistory) throws SQLException, ClassNotFoundException {
+        Service service = new Service();
+        service.deleteAllWifi();
+
         PreparedStatement pstmt = null;
         Connection conn = null;
 
@@ -118,9 +122,12 @@ public class Service {
     }
 
     public void calculateAndSaveDistance(RequestDistance requestDistance) {
+        System.out.println("===[calculateAndSaveDistance()]===");
         Float myLat = requestDistance.getLAT();
         Float myLnt = requestDistance.getLNT();
         int count = 0;
+
+        System.out.println("myLat, myLnt = " + myLat + ", " + myLnt);
 
         Connection conn = null;
         PreparedStatement pstmt = null;
@@ -133,19 +140,24 @@ public class Service {
             pstmt = conn.prepareStatement(sql);
             rs = pstmt.executeQuery();
             while (rs.next() && count < 500) {
+                // not here
                 float wifiLat = rs.getFloat("lat");
                 float wifiLnt = rs.getFloat("lnt");
+                System.out.println("wifiLat, wifiLnt = " + wifiLat + ", " + wifiLnt);
 
-                // calculate distance
+                //
                 double distance = calculateDistance(myLat, myLnt, wifiLat, wifiLnt);
+                System.out.println("calculated distance = " + distance);
 
                 ResponseDistance responseDistance = ResponseDistance.builder()
                         .distance(distance)
                         .manageNum(rs.getString("manage_num"))
                         .build();
 
+                System.out.println("responseDistance = " + responseDistance);
+
                 // save distance
-                saveDistance(responseDistance);
+                saveDistance(responseDistance); // 확인
                 count++;
             }
         } catch (SQLException e) {
@@ -170,6 +182,8 @@ public class Service {
     }
 
     private static double calculateDistance(Float x1, Float y1, Float x2, Float y2) {
+        // not here
+        System.out.println("===[calculateDistance()]===");
         double dy = Math.pow(Math.abs(y2 - y1), 2);
         double dx = Math.pow(Math.abs(x2 - x1), 2);
 
@@ -177,6 +191,7 @@ public class Service {
     }
 
     public static void saveDistance(ResponseDistance responseDistance) {
+        System.out.println("===[saveDistance()]===");
         PreparedStatement pstmt = null;
         Connection conn = null;
 
@@ -215,6 +230,7 @@ public class Service {
     }
 
     public List<ResponseWifi> getWifi20() {
+        System.out.println("===[getWifi20()]===");
         List<ResponseWifi> responseWifi20 = new ArrayList<>();
 
         Connection conn = null;
@@ -282,13 +298,13 @@ public class Service {
 
         try {
             conn = dbConn.getConn();
-            String sql = "select * from history order by history_id desc";
+            String sql = "select * from history order by id desc";
             pstmt = conn.prepareStatement(sql);
             rs = pstmt.executeQuery();
 
             while (rs.next()) {
                 ResponseHistory responseHistory = ResponseHistory.builder()
-                        .id(rs.getInt("history_id"))
+                        .id(rs.getInt("id"))
                         .x(rs.getFloat("x"))
                         .y(rs.getFloat("y"))
                         .createdTime(rs.getTimestamp("created_time"))
@@ -296,6 +312,11 @@ public class Service {
 
                 responseHistories.add(responseHistory);
             }
+
+            for (ResponseHistory responseHistory : responseHistories) {
+                System.out.println("responseHistory 리스트" + responseHistory);
+            }
+
         } catch (SQLException e) {
             System.out.println("[SQL Error : " + e.getMessage() + "]");
         } finally {
